07582768b64c4611636a561e1933d2ee83c81d78
syn4z <iatcosorin797@gmail.com>
2023-02-21 21:04:43 +0200

    Small fix

--- Lab2/heapSort.py
+++ Lab2/heapSort.py
@@ -1,14 +1,19 @@
+# This function arranges the elements of an array into a heap
+# It takes three arguments: the array, its size, and the index of the element to start heapify from
 def heapify(dataList, n, i):
-    largest = i
-    left = 2 * i + 1
-    right = 2 * i + 2
+    largest = i    # initialize the index of the largest element as i
+    left = 2 * i + 1    # get the index of the left child of the element at i
+    right = 2 * i + 2    # get the index of the right child of the element at i
 
+    # If the left child of i is larger than the largest element so far, update the largest index
     if left < n and dataList[left] > dataList[largest]:
         largest = left
 
+    # If the right child of i is larger than the largest element so far, update the largest index
     if right < n and dataList[right] > dataList[largest]:
         largest = right
 
+    # If the largest element is not i, swap the i-th and largest elements and recursively heapify the affected subtree
     if largest != i:
         dataList[i], dataList[largest] = dataList[largest], dataList[i]
         heapify(dataList, n, largest)
